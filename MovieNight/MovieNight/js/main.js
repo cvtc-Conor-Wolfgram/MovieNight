760092c45f170ec437734cf1c3e4361e195ca9e4
dkurschner1 <35713784+dkurschner1@users.noreply.github.com>
2019-04-06 12:15:58 -0500

    account info page added
    Added account info page. I made a few css tweaks that affect only the account info page. I didn't want to change the default without discussing with the group first.

---
+++ MovieNight/MovieNight/js/main.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(() => {
+    let imdbNumber = 'tt2015381';
+    favoriteMovie(imdbNumber);
+    
+ });
+
+function favoriteMovie(imdbNumber) {
+
+    axios.get('http://www.omdbapi.com?i=' + imdbNumber + '&apikey=b9bb3ece')
+        .then((response) => {
+            console.log(response);
+            let movies = response.data.Poster;
+            let output = '';
+
+            output += `
+                <img src="${movies}">
+            `;
+           
+
+            $('#accountImage').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
